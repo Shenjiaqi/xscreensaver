e950d1ee8e4276eedc8d10d568bcb2a85a7c02a8
shenjiaqi <1029393956@qq.com>
2016-03-27 13:42:16 +0800

    add build script for mac env

---
+++ build/doMake.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+export PATH=$PATH://usr/local/Cellar/gettext/0.19.7/bin/
+pixbuf="usr/local/Cellar/gdk-pixbuf/2.32.3/lib/gdk-pixbuf-2.0/2.10.0/loaders/"
+gtk_lib="-L/usr/local/Cellar/gtk+/2.24.30/lib/ -L/usr/local/Cellar/gtk+3/3.18.9/lib/ -L/usr/local/Cellar/GDK-Pixbuf/2.32.3/lib/"
+gtk_include="-I/usr/local/Cellar/gtk+/2.24.30/include/ -I/usr/local/Cellar/gtk+3/3.18.9/include/ -I/usr/local/Cellar/GDK-Pixbuf/2.32.3/include"
+export PKG_CONFIG_PATH=/opt/X11/lib/pkgconfig
+../configure CFLAGS="${gtk_include} \
+-I/opt/X11/include \
+-I/usr/local/Cellar/gdk-pixbuf/2.32.3/include/gdk-pixbuf-2.0/" \
+LDFLAGS="${gtk_lib} \
+-L/usr/local/Cellar/gettext/0.19.3/lib/ \
+-L/usr/local/Cellar/gdk-pixbuf/2.32.3/lib/" \
+CPPFLAGS="${gtk_include} \
+-I/opt/X11/include \
+-I/usr/local/Cellar/gettext/0.19.3/include/ \
+-I/usr/local/Cellar/gdk-pixbuf/2.32.3/include/gdk-pixbuf-2.0/"
